ece3df4c8a43bd3d0ed508eeb519ca7420221c36
ArokiyaVijay <101968597+ArokiyaVijay@users.noreply.github.com>
2022-03-20 14:08:23 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,29 @@
+package assignment1.week1;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=13;
+		boolean flag=false;
+		for (int i = 2; i <= num/2; i++) 
+		{
+			if (num%i==0)
+			{
+				flag=true;
+				break;
+			}
+			
+		}
+		if (!flag)
+		{
+			System.out.println( num+" is a prime num");
+		}
+		else
+		{
+			System.out.println(num+" Not a prime num");
+		}
+	}
+
+
+}
